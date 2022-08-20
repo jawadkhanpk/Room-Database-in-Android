28b80c7ae5e90b9e9f2de1ce1c9a69ed55418911
jawadkhanpk <jawadkhan.tech@gmail.com>
2022-08-20 12:34:52 +0500

    Insertion in Room Database

--- app/src/main/java/com/example/roomdatabase/MainActivity.java
+++ app/src/main/java/com/example/roomdatabase/MainActivity.java
@@ -1,15 +1,49 @@
 package com.example.roomdatabase;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    EditText etFirstName, etLastName;
+    Button btnInsertToRoom;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
+        etFirstName = findViewById(R.id.idEtFirstName);
+        etLastName = findViewById(R.id.idEtLastName);
+        btnInsertToRoom = findViewById(R.id.idBtnInsertToRoom);
+
+        btnInsertToRoom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new BgThread().start();
+            }
+        });
     }
-}
+        class BgThread extends Thread{
+            public  void run(){
+             super.run();
+
+                AppDatabase db = Room.databaseBuilder(getApplicationContext(),
+                        AppDatabase.class, "room_db").build();
+
+                UserDao userDao = db.userDao();
+                userDao.insertRecord(new User(8, etFirstName.getText().toString(),etLastName.getText().toString()));
+//                etFirstName.setText("");
+//                etLastName.setText("");
+
+            }
+        }
+    }
